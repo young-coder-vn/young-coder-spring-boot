d96d6aa3d7157d57513e4bf22a2152bcb4335253
Nguyễn Hưng <hungnv1@vnpay.vn>
2020-08-10 09:37:23 +0700

    add

---
+++ qr-test/cloud-config-client/src/main/java/com/young/coder/config/cloud/client/ClientService.java
@@ -0,0 +1,11 @@
+package com.young.coder.config.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientService {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientService.class, args);
+    }
+}
